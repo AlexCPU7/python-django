a6ed266da9a42bd3422aab03a5bcbcbb4f342f75
Alex <alexcpu7@gmail.com>
2018-08-15 21:25:25 +0300

    add comments tables

--- news/admin.py
+++ news/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from news.models import News, Category, Tag
+from news.models import News, Category, Tag, Comments
 
 # Register your models here.
 
 admin.site.register(News)
 admin.site.register(Category)
 admin.site.register(Tag)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'new', 'created', 'moderation')
+
+admin.site.register(Comments, CommentAdmin)
--- news/models.py
+++ news/models.py
@@ -4,7 +4,6 @@ from ckeditor.fields import RichTextField
 
 User = get_user_model()
 
-# Create your models here.
 
 class Category(models.Model):
     title = models.CharField('Заголовок', max_length=50)
@@ -27,9 +26,11 @@ class Tag(models.Model):
     def __str__(self):
         return self.title
 
+
 class News(models.Model):
-    user = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE)        #при удалении user строка не удалится
-    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.SET_NULL,
+                                 null=True)  # при удалении строка не удалится
     title = models.CharField('Заголовок', max_length=100)
     text_anons = RichTextField('Текст (анонс)', max_length=350)
     text = RichTextField('Текст статьи')
@@ -43,6 +44,20 @@ class News(models.Model):
         verbose_name_plural = 'Статьи'
 
     def __str__(self):
-        #return self.title
-        return 'Автор %s название %s'%(self.user, self.title)
+        # return self.title
+        return 'Автор %s название %s' % (self.user, self.title)
 
+
+class Comments(models.Model):
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    new = models.ForeignKey(News, verbose_name='Новость', on_delete=models.CASCADE)
+    text = RichTextField('Комментарий')
+    created = models.DateTimeField('Дата добавления', auto_now_add=True, null=True)
+    moderation = models.BooleanField('Модерация', default=False)
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return "{}".format(self.user)
