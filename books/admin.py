24452b3ee33d000b284e5566868f921c536a96f2
Alex <alexcpu7@gmail.com>
2018-08-20 16:40:21 +0300

    testing filter in admin

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from books.models import Type, Author, Tag, Book
 
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type_id', 'year', 'pages', 'language', 'create_dt')
+    list_editable = ['type_id']
+    search_fields = ['title']  #tags__title
+    list_filter = ('type_id', 'year', 'language', 'tags',  'create_dt')
+
+
 admin.site.register(Type)
 admin.site.register(Author)
 admin.site.register(Tag)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
--- news/admin.py
+++ news/admin.py
@@ -3,11 +3,12 @@ from news.models import News, Category, Tag, Comments
 
 # Register your models here.
 
-admin.site.register(News)
-admin.site.register(Category)
-admin.site.register(Tag)
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('user', 'new', 'text', 'created', 'moderation')
 
+
+admin.site.register(News)
+admin.site.register(Category)
+admin.site.register(Tag)
 admin.site.register(Comments, CommentAdmin)
